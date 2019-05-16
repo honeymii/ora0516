e9e8c8eda8b9ea4ba3026cb824b1bb3642ba56be
Judit Szekeres <szekeresjuci1@gmail.com>
2019-05-16 10:06:25 +0000

    adds new methods

--- src/ora0516_git/Ora0516_git.java
+++ src/ora0516_git/Ora0516_git.java
@@ -5,10 +5,20 @@ package ora0516_git;
  * @author szekeresjudit
  */
 public class Ora0516_git {
+    
+    public static void method() {
+        System.out.println("Hello!!!");
+    }
 
     public static void main(String[] args) {
-        
         System.out.println("Hello GitHub!");
-
+    }
+    
+    public static void method2() {
+        System.out.println("HI!!!");
+    }
+    
+    public static void method3() {
+        System.out.println("Hey!!!");
     }
 }
